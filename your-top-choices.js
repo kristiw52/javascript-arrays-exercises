3fa3de6f74d56a537fddf870932863091e55311e
Kristi Wright <kristiw52@hotmail.com>
2017-01-02 12:18:18 -0800

    completing exercises

--- your-top-choices.js
+++ your-top-choices.js
@@ -10,3 +10,22 @@ picking the right suffix for the number based on what it is.
 */
 
 // YOUR CODE GOES HERE
+var topChoices = ['italian', 'mexican', 'thai', 'japanese'];
+for (var i = 0; i < topChoices.length; i++) {
+    console.log('My #' + (i + 1) + ' choice is ' + topChoices[i]);
+}
+
+for (var i = 0; i < topChoices.length; i++) {
+    var choiceNum = i + 1;
+    var choiceNumSuffix;
+    if (choiceNum == 1) {
+        choiceNumSuffix = 'st';
+    } else if (choiceNum == 2) {
+        choiceNumSuffix = 'nd';
+    } else if (choiceNum == 3) {
+        choiceNumSuffix = 'rd';
+    } else {
+        choiceNumSuffix = 'th';
+    }
+    console.log('My ' + choiceNum + choiceNumSuffix + ' choice is ' + topChoices[i]);
+}

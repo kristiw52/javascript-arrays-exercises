4d2d604a95c6755da0f752ebf06e002d3ef7705b
Kristi Wright <kristiw52@hotmail.com>
2017-01-02 12:32:14 -0800

    completing exercise

--- longest-string.js
+++ longest-string.js
@@ -8,3 +8,13 @@ hint: strings also have the .length property - i.e `"starburst".length` is 9
 */
 
 // YOUR CODE GOES HERE
+var colors = ["red", "orange", "magenta", "swampygreen"];
+var longestColor = "";
+
+for(var i = 0; i < colors.length; i++) {
+    var newColor = colors[i];
+    if(newColor.length > longestColor.length) {
+        longestColor = newColor;
+    }
+}
+console.log("The longest color is " + longestColor + ". It has " + longestColor.length + " letters.")
